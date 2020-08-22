a48f634d93843f0650069b25a8ce1f180d1423f3
Front <shokooh.jk@gmail.com>
2020-08-22 20:02:48 +0430

    fix more bugs and finish project at deadline

--- src/com/mapsa/comfy-house/MainProject.js
+++ src/com/mapsa/comfy-house/MainProject.js
@@ -1,169 +1,196 @@
-// let data = `{
-//     "items": [
-//       {
-//         "sys": { "id": "1" },
-//         "fields": {
-//           "title": "queen panel bed",
-//           "price": 10.99,
-//           "image": { "fields": { "file": { "url": "../../../images/product-1.jpeg" } } }
-//         }
-//       },
-//       {
-//         "sys": { "id": "2" },
-//         "fields": {
-//           "title": "king panel bed",
-//           "price": 12.99,
-//           "image": { "fields": { "file": { "url": "../../../images/product-2.jpeg" } } }
-//         }
-//       },
-//       {
-//         "sys": { "id": "3" },
-//         "fields": {
-//           "title": "single panel bed",
-//           "price": 12.99,
-//           "image": { "fields": { "file": { "url": "../../../images/product-3.jpeg" } } }
-//         }
-//       },
-//       {
-//         "sys": { "id": "4" },
-//         "fields": {
-//           "title": "twin panel bed",
-//           "price": 22.99,
-//           "image": { "fields": { "file": { "url": "../../../images/product-4.jpeg" } } }
-//         }
-//       },
-//       {
-//         "sys": { "id": "5" },
-//         "fields": {
-//           "title": "fridge",
-//           "price": 88.99,
-//           "image": { "fields": { "file": { "url": "../../../images/product-5.jpeg" } } }
-//         }
-//       },
-//       {
-//         "sys": { "id": "6" },
-//         "fields": {
-//           "title": "dresser",
-//           "price": 32.99,
-//           "image": { "fields": { "file": { "url": "../../../images/product-6.jpeg" } } }
-//         }
-//       },
-//       {
-//         "sys": { "id": "7" },
-//         "fields": {
-//           "title": "couch",
-//           "price": 45.99,
-//           "image": { "fields": { "file": { "url": "../../../images/product-7.jpeg" } } }
-//         }
-//       },
-//       {
-//         "sys": { "id": "8" },
-//         "fields": {
-//           "title": "table",
-//           "price": 33.99,
-//           "image": { "fields": { "file": { "url": "../../../images/product-8.jpeg" } } }
-//         }
-//       }
-//     ]
-//   }`
+let data = `{
+    "items": [
+      {
+        "sys": { "id": "1" },
+        "fields": {
+          "title": "queen panel bed",
+          "price": 10.99,
+          "image": { "fields": { "file": { "url": "../../../images/product-1.jpeg" } } }
+        }
+      },
+      {
+        "sys": { "id": "2" },
+        "fields": {
+          "title": "king panel bed",
+          "price": 12.99,
+          "image": { "fields": { "file": { "url": "../../../images/product-2.jpeg" } } }
+        }
+      },
+      {
+        "sys": { "id": "3" },
+        "fields": {
+          "title": "single panel bed",
+          "price": 12.99,
+          "image": { "fields": { "file": { "url": "../../../images/product-3.jpeg" } } }
+        }
+      },
+      {
+        "sys": { "id": "4" },
+        "fields": {
+          "title": "twin panel bed",
+          "price": 22.99,
+          "image": { "fields": { "file": { "url": "../../../images/product-4.jpeg" } } }
+        }
+      },
+      {
+        "sys": { "id": "5" },
+        "fields": {
+          "title": "fridge",
+          "price": 88.99,
+          "image": { "fields": { "file": { "url": "../../../images/product-5.jpeg" } } }
+        }
+      },
+      {
+        "sys": { "id": "6" },
+        "fields": {
+          "title": "dresser",
+          "price": 32.99,
+          "image": { "fields": { "file": { "url": "../../../images/product-6.jpeg" } } }
+        }
+      },
+      {
+        "sys": { "id": "7" },
+        "fields": {
+          "title": "couch",
+          "price": 45.99,
+          "image": { "fields": { "file": { "url": "../../../images/product-7.jpeg" } } }
+        }
+      },
+      {
+        "sys": { "id": "8" },
+        "fields": {
+          "title": "table",
+          "price": 33.99,
+          "image": { "fields": { "file": { "url": "../../../images/product-8.jpeg" } } }
+        }
+      }
+    ]
+  }`
 
-
-// class Product{
-//     constructor({
-//         id : id,
-//         title : title,
-//         price : price,
-//         image : image
-//     })
+class Product{
+    constructor({
+        id : id,
+        title : title,
+        price : price,
+        image : image
+    })
     
-//     {
-//     this.id = id;
-//     this.title = title;
-//     this.price = price;
-//     this.image = image;
-//     }
+    {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.image = image;
+    }
     
-//     render(){
+    render(){
 
-//       this.product = document.getElementById('product')
-//       this.productsCenter = document.getElementById('products-center'); 
-//       this.list = document.getElementById('img-container') 
-//       const article = builder.create('article').appendTo(this.productsCenter).className('product')
-//       const div = builder.create('div').appendTo(article).className('img-container')
-//       builder.create('img').appendTo(div).src(this.image).className('product-img')
-//       const button = builder.create('button').className('bag-btn').appendTo(div)
-//       builder.create('i').text('ADD TO CART').className('fas fa-shopping-cart')
-//       .on("click",  ()=> {
-//         new Cart().add(this.id, this.price,this.image,this.title)
-//       }).appendTo(button)
-//       builder.create('i').className('fas fa-shopping-cart').appendTo(button)
-//       builder.create('h3').text(this.title).appendTo(article)
-//     }
-// }
+      this.product = document.getElementById('product')
+      this.productsCenter = document.getElementById('products-center'); 
+      this.list = document.getElementById('img-container') 
+      const article = builder.create('article').appendTo(this.productsCenter).className('product')
+      const div = builder.create('div').appendTo(article).className('img-container')
+      builder.create('img').appendTo(div).src(this.image).className('product-img')
+      const button = builder.create('button').className('bag-btn').appendTo(div)
+      builder.create('i').text('ADD TO CART').className('fas fa-shopping-cart')
+      .on("click",  ()=> {
+        new Cart().add(this.id, this.price,this.image,this.title)
+      }).appendTo(button)
+      builder.create('i').className('fas fa-shopping-cart').appendTo(button)
+      builder.create('h3').text(this.title).appendTo(article)
+    }
+}
 
-// class ProductManager {    
-//   render(product) {
-//     product.render()
-//   }
-// }
+class ProductList{
+    render(product){
+        product.render()
+    }
+}
 
-// class CartItem{
-//   constructor(id,price,image,title){
-//     this.id = id
-//     this.price = price
-//     this.image = image
-//     this.title = title
-//     this.quantity = 1;
+class CartItem {
+    constructor(id, title, price, image){
+        this.id = id,
+        this.title = title,
+        this.price = price,
+        this.image = image,
+        this.quantity = 1
+    }
 
-//   }
+    inc(){
+        this.quantity++
+    }
+    dec(){
+        this.quantity--
+    }
+    remove(){}
 
-//   remove(){
-//   }
+}
 
-//   inc(){
-//     this.quantity++
-//   }
+cartItems = [];
 
-//   dec(){
-//     return --this.quantity;
-//   }
+class Cart {  
+  render(){
+    this.cartContent = document.querySelector(".cart-content")
+    const cartTotal = document.querySelector(".cart-total")
+    const cartItemsClass = document.querySelector(".cart-items")
+    this.cartContent.innerHTML = ''
+            
+
+    cartItems.forEach(cartItem => {
+      const divCartItem = builder.create("div").className("cart-item").appendTo(this.cartContent)
+      builder.create("img").src(cartItem.image).appendTo(divCartItem)
+      const innerDiv = builder.create("div").appendTo(divCartItem)
+      builder.create("h4").text(cartItem.title).appendTo(innerDiv)
+      builder.create("h5").text(cartItem.price).appendTo(innerDiv)
+      builder.create("span").text("remove").className("remove-item").appendTo(innerDiv)
+      const div2 = builder.create('div').appendTo(divCartItem)
+
+      builder.create('i').className('fas fa-chevron-up').appendTo(div2).onclick(this.inc(cartItem))
+      builder.create('p').className('item-amount').text(cartItem.quantity).appendTo(div2)
+      builder.create('i').className('fas fa-chevron-down').appendTo(div2)
+      console.log(cartItems)
+      })
+    }
+  
+
+  inc(cartItem){
+    const ddd = cartItems.find(dd => dd.id === cartItem.id)
+    ddd.quantity++
+    
+    }
+    
+
+  add(id, price,image,title){
+   const cartItem = new CartItem(id,title,price,image)
+   const ccc = cartItems.find(cItem => cItem.id === cartItem.id)
+   ccc ? this.inc(ccc) : cartItems.push(cartItem)
+   this.render()
+  }
+
+  toggle(){
+      const showCart = document.querySelector(".cart")
+      showCart.classList.contains("showCart")? showCart.classList.remove("showCart") : showCart.classList.add("showCart")
+
+      const overlay = document.querySelector(".cart-overlay")
+      overlay.classList.contains("transparentBcg")? overlay.classList.remove("transparentBcg") : overlay.classList.add("transparentBcg")
+  }
+}
 
 
-// }
+const productList = new ProductList();
 
-// cartItems = []
+data = JSON.parse(data)
+let products = data.items
+products.forEach(product => {
+  let id = product.sys.id
+  let title = product.fields.title
+  let price = product.fields.price
+  let image = product.fields.image.fields.file.url
+  productList.render(new Product({id:id,title:title,price:price,image:image}))
+})
 
-// class Cart {
-//   constructor(){
-//     this.cartContent = document.getElementsByClassName("cart-content")
-//     this.clsBtn = document.getElementsByClassName("close-cart")
-//     this.showQuantity = document.getElementsByClassName("cart-items")
-//     this.cartTotal = document.getElementsByClassName("cart-total")
-//   }
-//   clear(){}
+const openCart = document.getElementById("cart-btn")
+const closeCart = document.getElementById("close-cart")
 
-
-//   render(){}
-
-//   add(id, price,image,title){
-//     const cartItem = new CartItem(id,price,image,title); 
-//     const ccc =  cartItems.find(cItem =>  cItem.id ===  cartItem.id )
-//     ccc ? cartItem.inc(ccc.id): cartItems.push(cartItem)   
-//     this.render()
-//   }
-
-
-// }
-
-
-// const manager = new ProductManager();
-
-// data = JSON.parse(data)
-// let products = data.items
-// products.forEach(product => {
-//   let id = product.sys.id
-//   let title = product.fields.title
-//   let price = product.fields.price
-//   let image = product.fields.image.fields.file.url
-//   manager.render(new Product({id:id,title:title,price:price,image:image}))
-// })
+const cart = new Cart();
+openCart.addEventListener("click", ()=>{ cart.toggle()})
+closeCart.addEventListener("click", ()=>{ cart.toggle()})
--- src/com/mapsa/comfy-house/MainProject2.js
+++ src/com/mapsa/comfy-house/MainProject2.js
@@ -1,213 +1,216 @@
-let data = `{
-    "items": [
-      {
-        "sys": { "id": "1" },
-        "fields": {
-          "title": "queen panel bed",
-          "price": 10.99,
-          "image": { "fields": { "file": { "url": "../../../images/product-1.jpeg" } } }
-        }
-      },
-      {
-        "sys": { "id": "2" },
-        "fields": {
-          "title": "king panel bed",
-          "price": 12.99,
-          "image": { "fields": { "file": { "url": "../../../images/product-2.jpeg" } } }
-        }
-      },
-      {
-        "sys": { "id": "3" },
-        "fields": {
-          "title": "single panel bed",
-          "price": 12.99,
-          "image": { "fields": { "file": { "url": "../../../images/product-3.jpeg" } } }
-        }
-      },
-      {
-        "sys": { "id": "4" },
-        "fields": {
-          "title": "twin panel bed",
-          "price": 22.99,
-          "image": { "fields": { "file": { "url": "../../../images/product-4.jpeg" } } }
-        }
-      },
-      {
-        "sys": { "id": "5" },
-        "fields": {
-          "title": "fridge",
-          "price": 88.99,
-          "image": { "fields": { "file": { "url": "../../../images/product-5.jpeg" } } }
-        }
-      },
-      {
-        "sys": { "id": "6" },
-        "fields": {
-          "title": "dresser",
-          "price": 32.99,
-          "image": { "fields": { "file": { "url": "../../../images/product-6.jpeg" } } }
-        }
-      },
-      {
-        "sys": { "id": "7" },
-        "fields": {
-          "title": "couch",
-          "price": 45.99,
-          "image": { "fields": { "file": { "url": "../../../images/product-7.jpeg" } } }
-        }
-      },
-      {
-        "sys": { "id": "8" },
-        "fields": {
-          "title": "table",
-          "price": 33.99,
-          "image": { "fields": { "file": { "url": "../../../images/product-8.jpeg" } } }
-        }
-      }
-    ]
-  }`
+// let data = `{
+//     "items": [
+//       {
+//         "sys": { "id": "1" },
+//         "fields": {
+//           "title": "queen panel bed",
+//           "price": 10.99,
+//           "image": { "fields": { "file": { "url": "../../../images/product-1.jpeg" } } }
+//         }
+//       },
+//       {
+//         "sys": { "id": "2" },
+//         "fields": {
+//           "title": "king panel bed",
+//           "price": 12.99,
+//           "image": { "fields": { "file": { "url": "../../../images/product-2.jpeg" } } }
+//         }
+//       },
+//       {
+//         "sys": { "id": "3" },
+//         "fields": {
+//           "title": "single panel bed",
+//           "price": 12.99,
+//           "image": { "fields": { "file": { "url": "../../../images/product-3.jpeg" } } }
+//         }
+//       },
+//       {
+//         "sys": { "id": "4" },
+//         "fields": {
+//           "title": "twin panel bed",
+//           "price": 22.99,
+//           "image": { "fields": { "file": { "url": "../../../images/product-4.jpeg" } } }
+//         }
+//       },
+//       {
+//         "sys": { "id": "5" },
+//         "fields": {
+//           "title": "fridge",
+//           "price": 88.99,
+//           "image": { "fields": { "file": { "url": "../../../images/product-5.jpeg" } } }
+//         }
+//       },
+//       {
+//         "sys": { "id": "6" },
+//         "fields": {
+//           "title": "dresser",
+//           "price": 32.99,
+//           "image": { "fields": { "file": { "url": "../../../images/product-6.jpeg" } } }
+//         }
+//       },
+//       {
+//         "sys": { "id": "7" },
+//         "fields": {
+//           "title": "couch",
+//           "price": 45.99,
+//           "image": { "fields": { "file": { "url": "../../../images/product-7.jpeg" } } }
+//         }
+//       },
+//       {
+//         "sys": { "id": "8" },
+//         "fields": {
+//           "title": "table",
+//           "price": 33.99,
+//           "image": { "fields": { "file": { "url": "../../../images/product-8.jpeg" } } }
+//         }
+//       }
+//     ]
+//   }`
 
-class Product {
-    constructor(title,image,id,price){
-        this.title = title
-        this.image = image
-        this.id = id
-        this.price = price
-    }
+// class Product {
+//     constructor(title,image,id,price){
+//         this.title = title
+//         this.image = image
+//         this.id = id
+//         this.price = price
+//     }
     
-    render(){
-      this.product = document.getElementById('product')
-      this.productsCenter = document.getElementById('products-center'); 
-      this.list = document.getElementById('img-container') 
-      const article = builder.create('article').appendTo(this.productsCenter).className('product')
-      const div = builder.create('div').appendTo(article).className('img-container')
-      builder.create('img').appendTo(div).src(this.image).className('product-img')
-      const button = builder.create('button').className('bag-btn').appendTo(div)
-      .on("click",  ()=> {
-                new Cart().add(this.id, this.price,this.image,this.title)
-              })
-      builder.create('i').text('ADD TO CART').className('fas fa-shopping-cart')
-      .appendTo(button)
-      builder.create('i').className('fas fa-shopping-cart').appendTo(button)
-      builder.create('h3').text(this.title).appendTo(article)
-    }
-}
+//     render(){
+//       this.product = document.getElementById('product')
+//       this.productsCenter = document.getElementById('products-center'); 
+//       this.list = document.getElementById('img-container') 
+//       const article = builder.create('article').appendTo(this.productsCenter).className('product')
+//       const div = builder.create('div').appendTo(article).className('img-container')
+//       builder.create('img').appendTo(div).src(this.image).className('product-img')
+//       const button = builder.create('button').className('bag-btn').appendTo(div)
+//       .on("click",  ()=> {
+//                 new Cart().add(this.id, this.price,this.image,this.title)
+//               })
+//       builder.create('i').text('ADD TO CART').className('fas fa-shopping-cart')
+//       .appendTo(button)
+//       builder.create('i').className('fas fa-shopping-cart').appendTo(button)
+//       builder.create('h3').text(this.title).appendTo(article)
+//     }
+// }
+// class ProductList{ 
+//   render(product){
+//     product.render()
+//   }
+// }
 
-class ProductList{ 
-  render(product){
-    product.render()
-  }
-}
+// class CartItem {
+//   constructor(title,image,id,price){
+//     this.title = title
+//     this.image = image
+//     this.id = id
+//     this.price = price
+//     this.quantity = 1
+//   }
 
-class CartItem {
-  constructor(title,image,id,price){
-    this.title = title
-    this.image = image
-    this.id = id
-    this.price = price
-    this.quantity = 1
-  }
+//   render(cartItem){
+//     this.cartContent = document.querySelector(".cart-content")
+//     const divCartItem = builder.create("div").className("cart-item").appendTo(this.cartContent)
+//     builder.create("img").src(cartItem.image).appendTo(divCartItem)
+//     const innerDiv = builder.create("div").appendTo(divCartItem)
+//     builder.create("h4").text(cartItem.title).appendTo(innerDiv)
+//     builder.create("h5").text(cartItem.price).appendTo(innerDiv)
+//     builder.create("span").text("remove").className("remove-item").appendTo(innerDiv).on("click", ()=>{
+//       new CartItem().remove(cartItem)
+//     })
 
-  inc(){
-    this.quantity++
-  }
+//     const div2 = builder.create('div').appendTo(divCartItem)
 
-  dec(){
-    this.quantity--
-  }
+//     builder.create('i').className('fas fa-chevron-up').appendTo(div2).onclick(()=>{this.inc(cartItem)})
+//     builder.create('p').className('item-amount').text(cartItem.quantity).appendTo(div2)
+//     builder.create('i').className('fas fa-chevron-down').appendTo(div2).onclick(()=>{this.dec(cartItem)})
+//   }
 
-  render(){
+//   remove(cartItem){
+//     cartItems = cartItems.filter(c => c.id != cartItem.id)
+//     new Cart().render();
+//   }
 
-    this.cartItem = document.getElementsByClassName("cart-item")
-    this.cart = document.getElementById("cart")
+//   inc(cartItem){
+//     const car = cartItems.find(ca => ca.id === cartItem.id)
+//     car.quantity++
+//     new Cart().render()
+//   }
 
-
-    builder.create('img').src(product.image).appendTo(cartItem)
-    const div1 = builder.create('div').appendTo(cartItem)
-    builder.create('h4').text(product.title).appendTo(div1)
-    builder.create('h5').text(product.price).appendTo(div1)
-    builder.create('span').text(remove).className('remove-item').appendTo(div1)
-
-    const div2 = builder.create('div').appendTo(cartItem)
-    builder.create('i').className('fa-chevron-up').text('::before').appendTo(div2)
-    // .onclick(inc())
-    builder.create('p').className('item-amount')
-    // .text(this.quantity)
-    builder.create('i').className('fa-chevron-down').text('::before').appendTo(div2)
-    // .onclick(dec())
-    const content = builder.create('div').className("cart-content").appendTo(this.cart)
-    // 
-    const cartItem = builder.create('div').className('cart-item').appendTo(content)
-
-  }
-
-  remove(){}
-
-}
-
-cartItems = []
-
-class Cart {
-  constructor(){
-  }
-
-  render(){
-    this.main = document.getElementsByClassName("cart-overlay")
-    this.cart = document.getElementById("cart")
-  
-    builder.create('div').className('cart').appendTo(this.main)
-    const span = builder.create('span').className("close-cart").appendTo(this.cart)
-    builder.create('i').className("fas fa-window-close").text('::before').appendTo(span)
-    builder.create('h2').text('Your Cart').appendTo(this.cart)
-
-    const footer = builder.create('div').className("cart-footer").appendTo(this.cart)
-    const h3 = builder.create('h3').text('your total : $').appendTo(footer)
-    // 
-    builder.create('span').className('cart-total').appendTo(h3)
-    builder.create('button').className('clear-cart banner-btn').text('clear cart').appendTo(footer)
-  }
-
-  clearCart(){}
-
-  add(id, price, image, title) {
-    const cartItem = new CartItem(title,image,id,price)
-    const ccc = cartItems.find(cItem => cItem.id === cartItem.id)
-    ccc ? cartItem.inc(ccc.id) : cartItems.push(cartItem)
+//   dec(cartItem){
     
-  }
+//     if(cartItem.quantity == 1){
+//       cartItems = cartItems.filter(cItem => cItem.id != cartItem.id)
+//     }else{
+//     const car = cartItems.find(ca => ca.id === cartItem.id)
+//     car.quantity--
+//     }
+//     new Cart().render()
 
-  toggle(){
-    const showCart = document.querySelector(".cart");
-    showCart.classList.contains("showCart") ? showCart.classList.remove("showCart") : showCart.classList.add("showCart")
+//   }
+// }
 
-    const cartOverLay = document.querySelector(".cart-overlay");
-    cartOverLay.classList.contains("transparentBcg") ? cartOverLay.classList.remove("transparentBcg") : cartOverLay.classList.add("transparentBcg")
-  
-  }
+// cartItems = []
 
-  totalPrice(){}
+// class Cart {
 
-}
+//   render(){
+//     const cartTotal = document.querySelector(".cart-total")
+//     const cartItemsClass = document.querySelector(".cart-items")
+//     var amount = 0
+//     var countItem = 0
+
+//     this.cartContent = document.querySelector(".cart-content")
+//     this.cartContent.innerHTML = ''; 
+
+//     cartItems.forEach(cartItem => {
+//       cartItem.render(cartItem)
+//       amount += cartItem.price * cartItem.quantity
+//       countItem += cartItem.quantity
+//     })
+
+//     cartItemsClass.textContent = countItem
+//     cartTotal.textContent = amount
+//   }
+
+//   inc(cartItem){
+//     cartItem.quantity++
+//   }
+
+//   add(id, price, image, title) {
+//     const cartItem = new CartItem(title,image,id,price)
+//     const existCartItem = cartItems.find(cItem => cItem.id === cartItem.id)
+//     existCartItem ? this.inc(existCartItem) : cartItems.push(cartItem)    
+//     this.render()
+//   }
+
+//   toggle(){
+//     const showCart = document.querySelector(".cart");
+//     showCart.classList.contains("showCart") ? showCart.classList.remove("showCart") : showCart.  classList.add("showCart")
+
+//     const cartOverLay = document.querySelector(".cart-overlay");
+//     cartOverLay.classList.contains("transparentBcg") ? cartOverLay.classList.remove("transparentBcg") : cartOverLay.classList.add("transparentBcg")
+
+//     this.render()
+//   }
+// }
 
 
-const productList = new ProductList()
-data = JSON.parse(data)
-items = data.items
-items.forEach(item => {
-  id = item.sys.id,
-  title = item.fields.title,
-  image = item.fields.image.fields.file.url,
-  price = item.fields.price
-
-  productList.render(new Product(title,image,id,price))
-})
-
+// const productList = new ProductList()
+// data = JSON.parse(data)
+// items = data.items
+// items.forEach(item => {
+//   id = item.sys.id,
+//   title = item.fields.title,
+//   image = item.fields.image.fields.file.url,
+//   price = item.fields.price
+//   productList.render(new Product(title,image,id,price))
+// })
 
 
-const openCart = document.getElementById("cart-btn")
-
-const closeCart = document.getElementById("close-cart")
-openCart.addEventListener("click", ()=>{ new Cart().toggle()})
-closeCart.addEventListener("click", ()=>{ new Cart().toggle()})
-
-
+// const openCart = document.getElementById("cart-btn")
+// const closeCart = document.getElementById("close-cart")
+// const clearCart = document.querySelector(".clear-cart")
+// const cart = new Cart();
+// openCart.addEventListener("click", ()=>{ cart.toggle()})
+// closeCart.addEventListener("click", ()=>{ cart.toggle()})
+// clearCart.addEventListener("click" , ()=> {cartItems = [], cart.render()})
